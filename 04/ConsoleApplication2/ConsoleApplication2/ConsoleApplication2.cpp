adb56a5dcd0d55f58a14a5e55dd07400fe56abb2
xie.kunming <xie.kunming@astute-tec.com>
2020-04-11 10:38:04 +0800

    add 04

---
+++ 04/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+
+
+
+void f()
+{
+    
+}
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+
+    if (a > b)
+    {
+        printf("a>b\n");
+    }
+    else
+    {
+        printf("a<=b\n");
+    }
+
+    f();
+    printf("hello world\n");
+    return 0;
+}
