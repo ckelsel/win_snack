3b695913f0e61f1ae776f0c616d39671d5bc6979
xie.kunming <xie.kunming@astute-tec.com>
2020-04-11 17:18:21 +0800

    add 05

---
+++ 05/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,43 @@
+﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+
+
+#define MAP_WIDTH 60
+#define MAP_HEIGHT 20
+
+int main()
+{
+    //[0,21)
+    for (int j = 0; j < MAP_HEIGHT + 1; j++)
+    {
+        if (j == MAP_HEIGHT)
+        {
+            // 绘制一行
+            for (int i = 0; i < MAP_WIDTH; i++)
+            {
+                printf("-");
+            }
+            printf("\n");
+
+        }
+        else
+        {
+            // 绘制空行
+            for (int i = 0; i < MAP_WIDTH; i++)
+            {
+                printf(" ");
+            }
+            printf("|");
+            printf("\n");
+        }
+        
+    }
+    
+    while (1)
+    {
+
+    }
+    return 0;
+}
